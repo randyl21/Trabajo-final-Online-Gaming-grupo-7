6b9ccea308aff2107cab27f6b6afb8e3dd78eb0a
randyl21 <randylcondem@gmail.com>
2026-03-23 22:14:59 -0400

    se hace una prueba para TF

---
+++ app.py
@@ -0,0 +1,11 @@
+# Importación de librerías necesarias
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# 1. Configuración de página
+st.set_page_config(
+    page_title="Dashboard de Inmuebles",
+    page_icon="🏠",
+    layout="wide"
+)
